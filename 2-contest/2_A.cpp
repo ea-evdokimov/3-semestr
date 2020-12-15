d1d99e810fb84685307b9c4671d540e9688ca375
ea-evdokimov <evdokimov.ea@phystech.edu>
2020-12-15 19:27:44 +0300

    runid - 45535240

--- 2-contest/2_A.cpp
+++ 2-contest/2_A.cpp
@@ -2,39 +2,46 @@
 #include <vector>
 
 template<typename T>
-struct Point {
+struct Point{
     T x, y;
+    friend std::istream& operator>> (std::istream &in, Point<T> &p)
+    {
+        in >> p.x;
+        in >> p.y;
+        return in;
+    }
 };
 
 template<typename T>
-struct Vector {
+struct Vector{
     T x, y;
 
-    Vector(T x1, T y1, T x2, T y2) : x(x2 - x1), y(y2 - y1) {}
-
+    Vector(T x1, T y1, T x2, T y2){
+        Vector(Point<T>{x1, y1}, Point<T>{x2, y2});
+    }
     Vector(const Point<T> &a, const Point<T> &b) : x(b.x - a.x), y(b.y - a.y) {}
 
-    T cross_product(const Vector &b) {
+    T cross_product(const Vector &b){
         //векторное произведение
         return x * b.y - y * b.x;
     }
 
-    T len() {
+    T len(){
         return x * x + y * y;
     }
 
-    T dot_product(const Vector &b) {
-        return x * b.x + y * b.y;
+    T dot_product(const Vector &b){
+        return x * b.x  + y * b.y;
     }
 };
 
 template<typename T>
-struct Cut {
+struct Cut{
     Point<T> a, b;
 };
 
 template<typename T>
-class Cuts {
+class Cuts{
 private:
     Cut<T> trace;
     std::vector<Cut<T>> rivers;
@@ -45,55 +52,54 @@ public:
 };
 
 template<typename T>
-int Cuts<T>::intersects() {
+int Cuts<T>::intersects(){
     Vector<T> v12{trace.a, trace.b};
     int res = 0;
 
-    for (auto cur_cut : rivers) {
+    for(auto cur_cut : rivers){
         Vector<T> v34{cur_cut.a, cur_cut.b}, v31{cur_cut.a, trace.a}, v32{cur_cut.a, trace.b},
                 v13{trace.a, cur_cut.a}, v14{trace.a, cur_cut.b};
 
         //отдельно случай, где река вдоль реки
-        if (v12.cross_product(v34) == 0) {
+        if(v12.cross_product(v34) == 0){
             continue;
         }
 
-        if (v34.cross_product(v31) * v34.cross_product(v32) < 0 &&
-            v12.cross_product(v13) * v12.cross_product(v14) < 0) {
+        if(v34.cross_product(v31) * v34.cross_product(v32) < 0 && v12.cross_product(v13) * v12.cross_product(v14) < 0) {
             ++res;
             continue;
         }
 
-        if (v34.cross_product(v31) == 0) {
+        if(v34.cross_product(v31) == 0) {
             //проверка на то, что точка 1 принадлежит отрезку 34
             //достаточно чтобы (v31, v34) и (v14, v34) были >= 0
-            if (v31.dot_product(v34) >= 0 && v34.dot_product(v14) >= 0) {
+            if(v31.dot_product(v34) >= 0 && v34.dot_product(v14) >= 0){
                 ++res;
                 continue;
             }
         }
 
-        if (v34.cross_product(v32) == 0) {
+        if(v34.cross_product(v32) == 0) {
             //проверка на то, что точка 2 принадлежит отрезку 34
             Vector<T> v24{trace.b, cur_cut.b};
-            if (v32.dot_product(v34) >= 0 && v34.dot_product(v24) >= 0) {
+            if(v32.dot_product(v34) >= 0 && v34.dot_product(v24) >= 0){
                 ++res;
                 continue;
             }
         }
 
-        if (v12.cross_product(v13) == 0) {
+        if(v12.cross_product(v13) == 0) {
             //проверка на то, что точка 3 принадлежит отрезку 12
-            if (v31.dot_product(v12) <= 0 && v32.dot_product(v12) >= 0) {
+            if(v31.dot_product(v12) <= 0 && v32.dot_product(v12) >= 0){
                 ++res;
                 continue;
             }
         }
 
-        if (v12.cross_product(v14) == 0) {
+        if(v12.cross_product(v14) == 0) {
             //проверка на то, что точка 4 принадлежит отрезку 12
             Vector<T> v24{trace.b, cur_cut.b};
-            if (v14.dot_product(v12) >= 0 && v24.dot_product(v12) <= 0) {
+            if(v14.dot_product(v12) >= 0 && v24.dot_product(v12) <= 0){
                 ++res;
                 continue;
             }
@@ -106,13 +112,14 @@ int Cuts<T>::intersects() {
 int main() {
     std::vector<Cut<int64_t>> v;
     size_t N;
-    int64_t ax, ay, bx, by;
-    std::cin >> ax >> ay >> bx >> by >> N;
+    Point<int64_t> a, b;
+    std::cin >> a >> b;
+    std::cin >> N;
 
-    Cut<int64_t> trace{Point<int64_t>{ax, ay}, Point<int64_t>{bx, by}};
-    for (size_t i = 0; i < N; ++i) {
-        std::cin >> ax >> ay >> bx >> by;
-        v.push_back(Cut<int64_t>{Point<int64_t>{ax, ay}, Point<int64_t>{bx, by}});
+    Cut<int64_t> trace{a, b};
+    for(size_t i = 0; i < N; ++i){
+        std::cin >> a >> b;
+        v.push_back(Cut<int64_t>{a, b});
     }
 
     Cuts<int64_t> C(trace, v);
